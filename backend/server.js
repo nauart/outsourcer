81c8c912aaf5ae76cc4ee140f6bb10037eb34df0
Artem Nosach <nau.workabout@gmail.com>
2014-10-18 00:05:27 +0300

    Add server functionality.

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,21 @@
-var http = require('http');
-var server = http.createServer();
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var logger = require('./logger');
 
-server.listen(3030);
+var app = express();
+
+logger.info('Server is running on port 3030');
+
+var staticPath = path.normalize(__dirname + '/../public');
+app.use(express.static(staticPath));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+var routes = require('./routes')(app);
+var server = app.listen(3030);
+
+module.exports = app;
